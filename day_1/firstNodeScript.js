1591410ce7fb3fc49a828ed598c4009c62d61f2e
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-08-05 17:48:04 +0200

    Create first Node.js script

---
+++ day_1/firstNodeScript.js
@@ -0,0 +1,9 @@
+// firstNodeScript.js
+
+const name = "Samson";
+
+// Get Current Date
+const currentDate = new Date().toLocaleDateString();
+
+// Print the name and current date using console.log
+console.log(`Hello, my name is ${name} and today's date is ${currentDate}.`);
