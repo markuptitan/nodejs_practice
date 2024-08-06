5667c4e4af1f3cf0b5ea0ec35ecf8d291083a9e3
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-08-06 13:20:35 +0200

    Add app.js that tests math.js

---
+++ day_2/simple_calculator/app.js
@@ -0,0 +1,14 @@
+const { add, multiply, divide, minus } = require("./math");
+// Test add function
+add(1, 3, 4, 5, 5);
+add(7, 4, 4, 5, 6);
+
+// Test multiply function
+multiply(12, 12, 23, 12, 23);
+multiply(16, 12, 45, 21);
+
+// Test divide function
+divide(12, 6, 0.2, 5, 0.05);
+
+// Test minus function
+minus(100, 54, 26, 74, -321);
